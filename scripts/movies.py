98e448a5969d3347ea78c5b471df80eaea0285b3
Thomas Hirtz <hirtzt@gmail.com>
2024-01-24 09:49:20 +0100

    Update movies.py

--- scripts/movies.py
+++ scripts/movies.py
@@ -1,22 +1,39 @@
+from typing import Dict
+
 from clipboard import copy
 
-from renaiming import generate_prompt, list_contents, rename_items_with_checks
+from renaiming import (
+    generate_llm_prompt,
+    list_directory_items,
+    rename_items_with_checks,
+)
 
 if __name__ == "__main__":
-    directory = r""  # Replace with your directory path
+    directory = r"D:\Thomas\Desktop\Avenir LT Std"
 
-    # For generating prompt
-    items = list_contents(directory)
-    custom_instructions = """
+    # Generate a prompt for LLM to suggest new names for the items
+    items = list_directory_items(
+        directory=directory,
+        depth=-1,
+        include_folders=True,
+        include_files=True,
+    )
+
+    renaming_instructions = """
     Keep year in parenthesis. Examples:
     - 'Movie.Title.2020.mkv' => 'Movie Title (2020)'
     - 'Document_2021.pdf' => 'Document (2021)'
     """
-    prompt = generate_prompt(items, custom_instructions)
-    print(prompt)
-    copy(prompt)
 
-    # For renaming - this part is executed when clean names are provided
-    clean_names = []
+    llm_prompt = generate_llm_prompt(
+        items=items,
+        custom_instructions=renaming_instructions,
+    )
+    print(llm_prompt)
+    copy(llm_prompt)  # Copy the prompt to the clipboard for easy use
 
-    rename_items_with_checks(directory, clean_names)
+    # Prepare the renaming map with provided clean names
+    renaming_map: Dict[str, str] = {}
+
+    # Execute the renaming process using the specified renaming map
+    rename_items_with_checks(directory=directory, renaming_map=renaming_map)
